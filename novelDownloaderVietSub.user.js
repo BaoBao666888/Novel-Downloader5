21335edd61d28684004e28ffbf8df9f751180485
BaoBao666888 <nguyenquocbaopl321@gmail.com>
2025-04-10 14:28:04 +0700

    447.5

--- novelDownloaderVietSub.user.js
+++ novelDownloaderVietSub.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name        novelDownloaderVietSub
 // @description Menu Download Novel hoặc nhấp đúp vào cạnh trái của trang để hiển thị bảng điều khiển
-// @version     3.5.447.4
+// @version     3.5.447.5
 // @author      dodying | BaoBao
 // @namespace   https://github.com/dodying/UserJs
 // @supportURL  https://github.com/dodying/UserJs/issues
