7475811f0f64d99f7b9ec6a45d2e4cf2ffdeca01
BaoBao666888 <nguyenquocbaopl321@gmail.com>
2025-11-17 17:05:28 +0700

    up 0.1.8

--- rename_chapters/main_ui.py
+++ rename_chapters/main_ui.py
@@ -1,5 +1,7 @@
 """Entry point cho giao diện Tkinter cổ điển."""
 
+import multiprocessing
+
 from app.ui.tk_app import main as tk_main
 
 
@@ -8,4 +10,5 @@ def main():
 
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     main()
