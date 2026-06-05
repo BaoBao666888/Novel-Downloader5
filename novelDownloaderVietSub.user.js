d7960e0ad07792a8a861097f92da76a3252c81e5
Nguyen Quoc Bao <nguyenquocbaopl321@gmail.com>
2026-06-05 19:22:37 +0700

    fix version

--- novelDownloaderVietSub.user.js
+++ novelDownloaderVietSub.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name        novelDownloaderVietSub
 // @description Menu Download Novel hoặc nhấp đúp vào cạnh trái của trang để hiển thị bảng điều khiển
-// @version     3.5.448.5.1
+// @version     3.5.448.6
 // @author      dodying | BaoBao
 // @namespace   https://github.com/BaoBao666888/Novel-Downloader5
 // @supportURL  https://github.com/BaoBao666888/Novel-Downloader5/issues
