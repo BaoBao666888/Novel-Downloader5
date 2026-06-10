43d5b470730bd5cf362cf37ddc9df522a666a2d3
BaoBao666888 <nguyenquocbaopl321@gmail.com>
2026-06-11 01:03:37 +0700

    feat: add biquge345 rule

--- novelDownloaderVietSub.user.js
+++ novelDownloaderVietSub.user.js
@@ -9483,6 +9483,23 @@ function decryptDES(encrypted, key, iv) {
             content: '#mycontent',
             elementRemove: 'script, style',
         },
+        { // https://www.biquge345.com
+            siteName: '笔趣阁345',
+            filter: () => {
+                if (window.location.host !== 'www.biquge345.com') return 0;
+                if (document.querySelector('div.border > ul.info > li > a')) return 1;
+                if (document.querySelector('#txt')) return 2;
+                return 0;
+            },
+            title: 'h1',
+            writer: 'div.xinxi > span.x1 > a',
+            intro: 'div.xinxi > div.x3',
+            cover: 'div.zhutu > img',
+            chapter: 'div.border > ul.info > li > a',
+            chapterTitle: 'h1',
+            content: '#txt',
+            elementRemove: 'p, script, style',
+        },
     ];
     Rule.template = [ // 模板网站
         { // http://www.xbiquge.la/54/54439/
