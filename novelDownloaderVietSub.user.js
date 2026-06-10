17a6649ba5e4520a9a2a8a9e04d3940fffa0b543
BaoBao666888 <nguyenquocbaopl321@gmail.com>
2026-06-11 01:01:17 +0700

    feat: add 630shu rule

--- novelDownloaderVietSub.user.js
+++ novelDownloaderVietSub.user.js
@@ -9413,6 +9413,26 @@ function decryptDES(encrypted, key, iv) {
             content: '.readcotent',
             elementRemove: 'iframe, ins, .ad_content, script, style',
         },
+        { // https://www.630shu.net
+            siteName: '630书',
+            filter: () => {
+                if (window.location.host !== 'www.630shu.net') return 0;
+                if (document.querySelector('.zjlist > dd > a')) return 1;
+                if (document.querySelector('#content')) return 2;
+                return 0;
+            },
+            title: '#info > h1',
+            writer: 'div.options > span.item:nth-child(1) > a',
+            intro: '#intro',
+            cover: '.img_in > img',
+            chapter: '.zjlist > dd > a',
+            chapterTitle: '.bookname h1, h1',
+            content: '#content',
+            elementRemove: 'script, style',
+            contentReplace: [
+                [/恋上你看书网 WWW\.630SHU\.NET ，最快更新.+最新章节！/g, ''],
+            ],
+        },
     ];
     Rule.template = [ // 模板网站
         { // http://www.xbiquge.la/54/54439/
