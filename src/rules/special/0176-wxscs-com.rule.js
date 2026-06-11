492fedbfe35717b0587057112fb22b814c6b1e3b
BaoBao666888 <nguyenquocbaopl321@gmail.com>
2026-06-11 11:53:24 +0700

    feat: add wxscs rule

---
+++ src/rules/special/0176-wxscs-com.rule.js
@@ -0,0 +1,52 @@
+// @rule-name: 万象书城
+// @rule-source: special
+(
+// @rule-begin
+        { // https://wxscs.com/book/* ; https://www.wxsck.com/book/*
+            siteName: '万象书城',
+            filter: () => {
+                if (!/^(www\.)?(wxscs|wxsck)\.com$/.test(window.location.host)) return 0;
+                if (document.querySelector('#all-chapter > div.panel > div.panel-body > div.row > div.item > a')) return 1;
+                if (document.querySelector('#cont-body')) return 2;
+                return 0;
+            },
+            title: 'h1',
+            writer: 'div.media-body > div.row > div > a',
+            intro: 'div.book-detail',
+            cover: (doc) => {
+                const src = $('img.book-img-middel', doc).first().attr('src') || '';
+                return src ? Rule.helpers.absoluteUrl(src, location.href) : '';
+            },
+            chapter: '#all-chapter > div.panel > div.panel-body > div.row > div.item > a',
+            chapterTitle: 'h1',
+            deal: async (chapter) => {
+                const helpers = Rule.helpers;
+                const visited = new Set();
+                const parts = [];
+                let title = chapter.title || '';
+                let pageUrl = chapter.url;
+                for (let i = 0; pageUrl && !visited.has(pageUrl) && i < 20; i++) {
+                    visited.add(pageUrl);
+                    const doc = await helpers.requestDoc(pageUrl);
+                    const $doc = $(doc);
+                    if (!title) title = $doc.find('h1').first().text().trim();
+                    const content = $doc.find('#cont-body').first().clone();
+                    content.find('script, div').remove();
+                    const html = (content.html() || '').trim();
+                    if (html) parts.push(html);
+                    const next = $doc.find('#content > div.row > div.text-center > a:last-of-type')
+                        .filter((idx, el) => $(el).text().includes('下一页'))
+                        .first()
+                        .attr('href');
+                    const nextUrl = next ? helpers.absoluteUrl(next, pageUrl) : '';
+                    pageUrl = nextUrl && nextUrl.includes('.html') ? nextUrl : '';
+                }
+                return {
+                    title,
+                    content: parts.join(''),
+                };
+            },
+            thread: 1,
+        }
+// @rule-end
+)
