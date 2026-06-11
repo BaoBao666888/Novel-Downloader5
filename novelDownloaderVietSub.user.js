404b0338b1bd9ced3ae04f16f0448dfb9afeae20
BaoBao666888 <nguyenquocbaopl321@gmail.com>
2026-06-11 11:28:38 +0700

    feat: add 60ksw rule

--- novelDownloaderVietSub.user.js
+++ novelDownloaderVietSub.user.js
@@ -10223,6 +10223,24 @@ function decryptDES(encrypted, key, iv) {
             },
             thread: 1,
         },
+        { // https://www.60ksw.com
+            siteName: '60看书',
+            url: '://www\\.60ksw\\.com/[^/]+/[^/]+/[^/]+/index\\.html',
+            chapterUrl: '://www\\.60ksw\\.com/[^/]+/[^/]+/[^/]+/[^/]+\\.html',
+            filter: () => {
+                if (window.location.host !== 'www.60ksw.com') return 0;
+                if (document.querySelector('#chapterlist li > a')) return 1;
+                if (document.querySelector('#content')) return 2;
+                return 0;
+            },
+            title: 'div.booktitle > h1',
+            writer: '#author',
+            intro: '#bookintro',
+            cover: '#bookimg img',
+            chapter: '#chapterlist li > a',
+            chapterTitle: 'h1, .booktitle > h1',
+            content: '#content',
+        },
     ];
     Rule.template = [ // 模板网站
         { // http://www.xbiquge.la/54/54439/
