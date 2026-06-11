4d8035e75834c25e2e3e83a78c57d2c1e21ede4c
BaoBao666888 <nguyenquocbaopl321@gmail.com>
2026-06-11 11:03:40 +0700

    feat: add shauthor rule

--- novelDownloaderVietSub.user.js
+++ novelDownloaderVietSub.user.js
@@ -9835,6 +9835,57 @@ function decryptDES(encrypted, key, iv) {
             },
             thread: 1,
         },
+        { // https://m.shauthor.com
+            siteName: '大众文学',
+            url: '://m\\.shauthor\\.com/info_[^/]+/$',
+            chapterUrl: '://m\\.shauthor\\.com/read_[^/]+/[^/]+(?:_\\d+)?\\.html',
+            filter: () => {
+                if (window.location.host !== 'm.shauthor.com') return 0;
+                if (document.querySelector('div.info_chapters > ul.p2:last-of-type > li > a')) return 1;
+                if (document.querySelector('#novelcontent')) return 2;
+                return 0;
+            },
+            title: 'h1',
+            writer: (doc) => $('div.tab > p.p1', doc).first().text().replace(/^作者：?/, '').trim(),
+            intro: (doc) => $('meta[property="og:description"]', doc).attr('content') || '',
+            cover: 'div.tu > img',
+            chapter: 'div.info_chapters > ul.p2:last-of-type > li > a',
+            chapterTitle: '#novelcontent',
+            deal: async (chapter) => {
+                const helpers = Rule.helpers;
+                const visited = new Set();
+                const parts = [];
+                let title = chapter.title || '';
+                let pageUrl = chapter.url;
+                const baseMatch = pageUrl.match(/\/([^/_.]+)(?:_\d+)?\.html(?:$|[?#])/);
+                const baseId = baseMatch && baseMatch[1];
+                for (let i = 0; pageUrl && !visited.has(pageUrl) && i < 10; i++) {
+                    visited.add(pageUrl);
+                    const doc = await helpers.requestDoc(pageUrl);
+                    const $doc = $(doc);
+                    if (!title) title = $doc.find('#novelcontent').prevAll('h1').first().text().trim();
+                    const content = $doc.find('#novelcontent').first().clone();
+                    content.find('script, style, p').remove();
+                    let html = content.html() || '';
+                    html = html
+                        .replace(/内容未完，下一页继续阅读/g, '')
+                        .replace(/shauthor|大众文学/g, '')
+                        .trim();
+                    if (html) parts.push(html);
+                    const next = $doc.find('div.page_chapter > ul > li:last-of-type > a')
+                        .filter((idx, el) => $(el).text().includes('下一页'))
+                        .first()
+                        .attr('href');
+                    const nextUrl = next ? helpers.absoluteUrl(next, pageUrl) : '';
+                    pageUrl = baseId && new RegExp(`/${baseId}_\\d+\\.html(?:$|[?#])`).test(nextUrl) ? nextUrl : '';
+                }
+                return {
+                    title,
+                    content: parts.join('<br />'),
+                };
+            },
+            thread: 1,
+        },
     ];
     Rule.template = [ // 模板网站
         { // http://www.xbiquge.la/54/54439/
