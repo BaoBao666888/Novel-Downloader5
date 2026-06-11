a2b05df0275661cdb5d2c1123af980d0e40a9143
BaoBao666888 <nguyenquocbaopl321@gmail.com>
2026-06-11 10:45:42 +0700

    feat: add zhenhunxiaoshuo rule

--- novelDownloaderVietSub.user.js
+++ novelDownloaderVietSub.user.js
@@ -9636,6 +9636,36 @@ function decryptDES(encrypted, key, iv) {
                 [/半夏小說，快樂很多/g, ''],
             ],
         },
+        { // https://www.zhenhunxiaoshuo.com
+            siteName: '镇魂小说网',
+            url: '://www\\.zhenhunxiaoshuo\\.com/[^/]+/$',
+            chapterUrl: '://www\\.zhenhunxiaoshuo\\.com/\\d+\\.html',
+            filter: () => {
+                if (window.location.host !== 'www.zhenhunxiaoshuo.com') return 0;
+                if (document.querySelector('.focusbox-title') && document.querySelector('article.excerpt > a')) return 1;
+                if (document.querySelector('article.article-content')) return 2;
+                return 0;
+            },
+            title: '.focusbox-title, h1',
+            writer: (doc) => {
+                const html = $('div.focusbox-text > p', doc).first().html() || '';
+                const firstLine = html.split(/<br\s*\/?>/i)[0] || '';
+                return firstLine.replace(/<[^>]+>/g, '').replace(/^作者：?/, '').trim();
+            },
+            intro: (doc) => {
+                const html = $('div.focusbox-text > p', doc).first().html() || '';
+                return html
+                    .split(/<br\s*\/?>/i)
+                    .slice(1)
+                    .map((line) => line.replace(/<[^>]+>/g, '').trim())
+                    .filter(Boolean)
+                    .join('\n');
+            },
+            chapter: 'article.excerpt > a',
+            chapterTitle: '.article-title, h1',
+            content: 'article.article-content',
+            elementRemove: 'script, style, iframe, ins, .adsbygoogle',
+        },
     ];
     Rule.template = [ // 模板网站
         { // http://www.xbiquge.la/54/54439/
